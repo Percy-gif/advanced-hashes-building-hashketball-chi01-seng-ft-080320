b0e72c6b8d635ebcce644c4c23917a6d7fcfec39
Percy Major <chipercy@hotmail.com>
2020-08-11 02:22:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
           assists:  12     
           steals: 3,      
           blocks: 1,      
-          slam_dunks: 1,  
+          slam_dunks: 1  
          }, 
          {
           name: "Reggies Evans",
